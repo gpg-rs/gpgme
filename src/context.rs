021047abfc10775125b8c7564b29a1dd51bcafc6
John Schug <john.ips.schug@gmail.com>
2018-01-17 13:43:49 -0800

    Fix test

--- src/context.rs
+++ src/context.rs
@@ -199,13 +199,10 @@ impl Context {
 
     #[inline]
     pub fn set_pinentry_mode(&mut self, mode: ::PinentryMode) -> Result<()> {
-        require_gpgme_ver! {
-            (1, 7) => {} else {
-                if (self.protocol() == Protocol::OpenPgp) &&
-                    !self.engine_info().check_version("2.1.0") {
-                    return Ok(());
-                }
-            }
+        if (mode != ::PinentryMode::Default) &&
+            (self.protocol() == Protocol::OpenPgp) &&
+                !self.engine_info().check_version("2.1.0") {
+            return Err(Error::UNSUPPORTED_OPERATION);
         }
         unsafe {
             return_err!(ffi::gpgme_set_pinentry_mode(self.as_raw(), mode.raw()));
--- tests/context.rs
+++ tests/context.rs
@@ -1,23 +1,28 @@
+#[macro_use]
 extern crate gpgme;
 #[macro_use]
 extern crate lazy_static;
 extern crate tempdir;
 
-use gpgme::Context;
-use gpgme::PinentryMode::Loopback;
+use gpgme::{Context, PinentryMode, Error};
 
 #[macro_use]
 mod support;
 
-test_case! {
-    test_pinentry_mode(test) {
-        let mode = Loopback;
-        let mut context: Context = test.create_context();
-
-        context.set_pinentry_mode(mode).expect("should work");
-
-        let context_ref: &Context = &context;
-
-        assert_eq!(mode, context_ref.pinentry_mode());
+require_gpgme_ver! {
+    (1, 4) => {
+        test_case! {
+            test_pinentry_mode(test) {
+                let mode = PinentryMode::Loopback;
+                let mut ctx = test.create_context();
+                match ctx.set_pinentry_mode(mode) {
+                    Ok(()) => {
+                        assert_eq!(mode, Context::pinentry_mode(&ctx));
+                    }
+                    Err(Error::UNSUPPORTED_OPERATION) => (),
+                    e @ Err(_) => fail_if_err!(e),
+                }
+            }
+        }
     }
 }
