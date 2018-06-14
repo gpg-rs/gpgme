5e59c614009a3ddd819c910a66fdd930cf9a72a8
John Schug <john.ips.schug@gmail.com>
2018-06-13 17:41:05 -0700

    Update build_helper submodule

--- gpgme-sys/build.rs
+++ gpgme-sys/build.rs
@@ -1,131 +1,96 @@
 extern crate cc;
-extern crate semver;
 
 use std::env;
 use std::ffi::OsString;
-use std::path::{Path, PathBuf};
-use std::process::{self, Command};
-use std::str;
-
-use semver::Version;
+use std::path::PathBuf;
+use std::process::Command;
 
 mod build_helper;
 
 use build_helper::*;
 
-const INCLUDED_VERSION: &str = "1.10.0";
-
 fn main() {
-    if let Err(_) = configure() {
-        process::exit(1);
-    }
+    Project::default().configure(|proj| {
+        if let Ok(mut c) = proj.try_env() {
+            let _ = c.try_detect_version("gpgme.h", &(proj.prefix.clone() + "_VERSION"))?;
+            return Ok(c);
+        }
+
+        if let Some(path) = get_env(proj.prefix.clone() + "_CONFIG") {
+            return try_config(&proj, path);
+        }
+
+        if let r @ Ok(_) = proj.try_build(build) {
+            return r;
+        }
+
+        try_config(&proj, "gpgme-config")
+    })
 }
 
-fn configure() -> Result<()> {
-    let path = get_env("GPGME_LIB_DIR");
-    let libs = get_env("GPGME_LIBS");
-    if path.is_some() || libs.is_some() {
-        let mode = match get_env("GPGME_STATIC") {
-            Some(_) => "static=",
-            _ => "",
-        };
-        if let Some(v) = get_env("GPGME_VERSION").as_ref().and_then(|s| s.to_str()) {
-            print_version(Version::parse(&v).or(Err(()))?);
-        }
-        for path in path.iter().flat_map(env::split_paths) {
-            println!("cargo:rustc-link-search=native={}", path.display());
-        }
-        for lib in env::split_paths(libs.as_ref().map(|s| &**s).unwrap_or("gpgme".as_ref())) {
-            println!("cargo:rustc-link-lib={}{}", mode, lib.display());
-        }
-        return Ok(());
+fn try_config<S: Into<OsString>>(proj: &Project, path: S) -> Result<Config> {
+    let path = path.into();
+    let mut cmd = path.clone();
+    cmd.push(" --version");
+    let version = output(Command::new("sh").arg("-c").arg(cmd))?;
+
+    let mut cmd = path;
+    cmd.push(" --cflags --libs");
+    if cfg!(unix) {
+        cmd.push(" --thread=pthread");
+    }
+    let mut config = proj.try_config(Command::new("sh").arg("-c").arg(cmd))?;
+    config.version = Some(version.trim().into());
+    Ok(config)
+}
+
+fn build(proj: &Project) -> Result<Config> {
+    if proj.target.contains("msvc") {
+        return Err(());
     }
 
-    if let Some(path) = get_env("GPGME_CONFIG") {
-        return try_config(path);
-    }
-
-    if !Path::new("libassuan/autogen.sh").exists() || !Path::new("gpgme/autogen.sh").exists() {
-        let _ = run(Command::new("git").args(&["submodule", "update", "--init"]));
-    }
     let _ = run(Command::new("git")
         .current_dir("gpgme")
         .args(&["apply", "../gpgme-remove-doc.patch"]));
 
-    try_build().or_else(|_| try_config("gpgme-config"))
-}
-
-fn print_version(v: Version) {
-    println!("cargo:version={}", v);
-    println!("cargo:version_major={}", v.major);
-    println!("cargo:version_minor={}", v.minor);
-    println!("cargo:version_patch={}", v.patch);
-}
-
-fn try_config<S: Into<OsString>>(path: S) -> Result<()> {
-    let path = path.into();
-    let mut cmd = path.clone();
-    cmd.push(" --version");
-    let version = Version::parse(&output(Command::new("sh").arg("-c").arg(cmd))?).or(Err(()))?;
-
-    let mut cmd = path;
-    if cfg!(unix) {
-        cmd.push(" --thread=pthread");
-    }
-    cmd.push(" --libs");
-    parse_linker_flags(&output(Command::new("sh").arg("-c").arg(cmd))?);
-    print_version(version);
-    Ok(())
-}
-
-fn try_build() -> Result<()> {
-    if target().contains("msvc") {
-        return Err(());
-    }
-
-    let gpgerror_root = PathBuf::from(env::var_os("DEP_GPG_ERROR_ROOT").ok_or(())?);
-    let config = Config::new("libassuan")?;
-    run(Command::new("sh")
-        .current_dir(&config.src)
-        .arg("autogen.sh"))?;
-    let mut cmd = config.configure()?;
+    let gpgerror_root = env::var_os("DEP_GPG_ERROR_ROOT").map(PathBuf::from);
+    let build = proj.new_build("libassuan")?;
+    run(Command::new("sh").current_dir(&build.src).arg("autogen.sh"))?;
+    let mut cmd = build.configure_cmd()?;
     cmd.arg("--disable-doc");
-    cmd.arg({
+    if let Some(p) = gpgerror_root.as_ref() {
         let mut s = OsString::from("--with-libgpg-error-prefix=");
-        s.push(msys_compatible(&gpgerror_root)?);
-        s
-    });
+        s.push(msys_path(&p)?);
+        cmd.arg(s);
+    }
     run(cmd)?;
-    run(config.make())?;
-    run(config.make().arg("install"))?;
+    run(build.make_cmd())?;
+    run(build.make_cmd().arg("install"))?;
 
-    let config = Config::new("gpgme")?;
-    run(Command::new("sh")
-        .current_dir(&config.src)
-        .arg("autogen.sh"))?;
-    let mut cmd = config.configure()?;
+    let build = proj.new_build("gpgme")?;
+    run(Command::new("sh").current_dir(&build.src).arg("autogen.sh"))?;
+    let mut cmd = build.configure_cmd()?;
     cmd.arg("--disable-languages");
     cmd.arg("--disable-gpg-test");
-    cmd.arg({
+    if let Some(p) = gpgerror_root.as_ref() {
         let mut s = OsString::from("--with-libgpg-error-prefix=");
-        s.push(msys_compatible(&gpgerror_root)?);
-        s
-    });
+        s.push(msys_path(&p)?);
+        cmd.arg(s);
+    }
     cmd.arg({
         let mut s = OsString::from("--with-libassuan-prefix=");
-        s.push(msys_compatible(&config.dst)?);
+        s.push(msys_path(&proj.out_dir)?);
         s
     });
     run(cmd)?;
-    run(config.make())?;
-    run(config.make().arg("install"))?;
+    run(build.make_cmd())?;
+    run(build.make_cmd().arg("install"))?;
 
-    println!(
-        "cargo:rustc-link-search={}",
-        config.dst.join("lib").display()
-    );
-    parse_libtool_file(config.dst.join("lib/libgpgme.la"))?;
-    println!("cargo:root={}", config.dst.display());
-    print_version(Version::parse(INCLUDED_VERSION).unwrap());
-    Ok(())
+    let mut config = build.config();
+    config.parse_libtool_file(proj.out_dir.join("lib/libgpgme.la"))?;
+    config.try_detect_version("gpgme.h", "GPGME_VERSION")?;
+    if let Some(p) = gpgerror_root {
+        config.include_dir.insert(p.join("include"));
+    }
+    Ok(config)
 }
